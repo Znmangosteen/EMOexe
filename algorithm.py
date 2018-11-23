1ede80b2a6d273894e231c829481715a8eab9ca2
Znmangosteen <Znmangosteen@hotmail.com>
2018-11-23 22:33:49 +0800

    GA差rule set的算子

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,16 @@
 import crossover, mutation, selection, random, init
 
 
-def NSGAII(p, gen_num, constant, size=100) -> list:
+def NSGAII(population, p, gen_num, constant, size=100) -> list:
     pc = p[0]
     pm = p[1]
     pMchi = p[2]
     N = constant[0]
-    population = init.initialization(size)
 
     # 进化gen_num代
     for j in range(gen_num):
         offspring_population = []
+        pareto_ranking(population)
 
         # 生成子代个体并放到offspring_population
         for i in range(size):
@@ -33,14 +33,58 @@ def NSGAII(p, gen_num, constant, size=100) -> list:
 
         # 合并父子代并选择
         population = merge_and_select(population, offspring_population, size)
+    pareto_ranking(population)
+    pareto_set = []
+    for solution in population:
+        if solution.pareto == 1:
+            pareto_set.append(solution)
+    return pareto_set
 
 
-def merge_and_select(population, offspring_population, size):
-    total_popution = population + offspring_population
+def merge_and_select(population: list, offspring_population: list, size):
+    total_population = population + offspring_population
+
+    pareto_ranking(total_population)
+    total_population.sort()
+
+    return total_population[:size]
 
 
+def pareto_ranking(population: list):
+    size = len(population)
+    n = [0] * size
+    S = [set()] * size
+    for i in range(size - 1):
+        for j in range(i + 1, size):
+            s1 = population[i]
+            s2 = population[j]
 
-    return total_popution[:size]
+            if (s1.fit1 >= s2.fit1 and s1.fit2 >= s2.fit2) and (s1.fit1 > s2.fit1 or s1.fit1 > s2.fit1):
+                n[i] += 1
+                S[i].add(j)
+            elif (s1.fit1 <= s2.fit1 and s1.fit2 <= s2.fit2) and (s1.fit1 < s2.fit1 or s1.fit1 < s2.fit1):
+                n[j] += 1
+                S[j].add(i)
+
+    F = set()
+    for i in range(size):
+        if n[i] == 0:
+            F.add(i)
+
+    H = set()
+    pareto_num = 1
+    while len(F) > 0:
+        for k in F:
+            population[k].pareto = pareto_num
+
+            for i in S[k]:
+                n[i] -= 1
+                if n[i] == 0:
+                    pass
+                    H.add(i)
+        F = H
+        H.clear()
+        pareto_num += 1
 
 
 def michigan(population: list, N, p):
--- exer4.py
+++ exer4.py
@@ -4,15 +4,16 @@ if __name__ == '__main__':
     # 初期个体数
     size = 100
     # 种群
-    population = []
+    # population = []
     # 生成并评价所有规则
-    init.initialization(population, size)
+    population = init.initialization(size)
+
 
     while True:
         # 遗传变异
         # Pittsburge
         child = []
-        child=algorithm.michigan(child)
+        child = algorithm.michigan(child)
         # 替换操作
         # 判断终止条件
         # 从所有世代中选择识别精度最高的种群作为最终识别器，删除无用规则
